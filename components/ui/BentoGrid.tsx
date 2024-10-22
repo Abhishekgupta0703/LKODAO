d7dd02ef593b3f984512994dbfcca5b4e6d72007
Abhishekgupta0703 <ak19250000@gmail.com>
2024-10-22 19:53:49 +0530

    "Replace `img` tags with `next/image` components in various components"

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -72,7 +73,7 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img
+            <Image
               src={img}
               alt={img}
               className={cn(
@@ -89,7 +90,7 @@ export const BentoGridItem = ({
           } `}
         >
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
               className="object-cover object-center w-full h-full"
